cefcfae4043f0eba08b6798e3102f6a2584d7279
unknown <shaheedkhan336@gmail.com>
2024-09-28 01:39:40 +0500

    copy work done

--- resources/views/team/report/commercial-invoice/add.blade.php
+++ resources/views/team/report/commercial-invoice/add.blade.php
@@ -596,7 +596,24 @@
 @endsection
 
 @section('addScript')
+<script>
+    // Upar se values neeche walay fields mein copy karne ka function
+    function copyShipperToShipTo() {
+        document.getElementById('name_ship_to').value = document.getElementById('name_shipper').value;
+        document.getElementById('address_ship_to').value = document.getElementById('address_shipper').value;
+        document.getElementById('city_ship_to').value = document.getElementById('city_shipper').value;
+        document.getElementById('country_ship_to').value = document.getElementById('country_shipper').value;
+        document.getElementById('phone_ship_to').value = document.getElementById('phone_shipper').value;
+    }
 
+    copyShipperToShipTo();
+    // Jab koi upar wale fields mein change kare to neeche update ho
+    document.getElementById('name_shipper').addEventListener('input', copyShipperToShipTo);
+    document.getElementById('address_shipper').addEventListener('input', copyShipperToShipTo);
+    document.getElementById('city_shipper').addEventListener('input', copyShipperToShipTo);
+    document.getElementById('country_shipper').addEventListener('input', copyShipperToShipTo);
+    document.getElementById('phone_shipper').addEventListener('input', copyShipperToShipTo);
+</script>
 <script>
 $(document).ready(function() {
 
